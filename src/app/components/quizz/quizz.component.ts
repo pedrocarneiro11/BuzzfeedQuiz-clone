c393f79782b6835392fc16e1fa58bbd8592ef58d
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-10-08 11:33:30 -0300

    estilizacao dos componentes

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class QuizzComponent implements OnInit {
 
   constructor() { }
+  TituloPergunta:String = 'Titulo da pergunta';
+  QuestaoSelecionada:String = 'Questao selecionada';
 
   ngOnInit(): void {
   }
