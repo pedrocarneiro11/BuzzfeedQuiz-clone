0e15f33c619fbc1046e7df8a2e358c402dee4a56
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-10-08 15:00:57 -0300

    commit

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
@@ -10,7 +11,7 @@ export class QuizzComponent implements OnInit {
   constructor() { }
   TituloPergunta:String = 'Titulo da pergunta';
 
-  Questao:any;
+  questions:any;
   QuestaoSelecionada:any = 'Questao selecionada';
 
   answers:string[] = [];
@@ -22,6 +23,35 @@ export class QuizzComponent implements OnInit {
   finished:boolean = false;
 
   ngOnInit(): void {
+    if(quizz_questions) [
+      this.finished = false,
+      this.TituloPergunta = quizz_questions.title,
+
+      this.questions = quizz_questions.questions,
+      this.QuestaoSelecionada = this.questions[this.questionIndex],
+
+      this.questionIndex = 0,
+      this.questionMaxIndex = this.questions.length,
+
+      console.log(this.questionIndex),
+      console.log(this.questionMaxIndex),
+    ]
+  }
+
+  playerChoose(value:string) {
+    this.answers.push(value);
+    this.nextStep();
+  }
+
+  nextStep() {
+    this.questionIndex += 1;
+
+    if(this.questionMaxIndex > this.questionIndex) {
+      this.QuestaoSelecionada = this.questions[this.questionIndex];
+    } else {
+      this.finished = true;
+      // TODO: VERIFICAR OPCAO GANHADORA
+    }
   }
 
 }
